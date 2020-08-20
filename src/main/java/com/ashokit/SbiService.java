06717421e963dd292e7007a91066afeeeac45bc1
sridharyadav666 <kookatlasridharyadav@gmail.com>
2020-08-20 23:42:28 +0530

    fixed deposite implementation

--- src/main/java/com/ashokit/SbiService.java
+++ src/main/java/com/ashokit/SbiService.java
@@ -12,6 +12,7 @@ public class SbiService {
 		
 		
 		
-		
+	System.out.println("your fixed amount will double after four years only");
+	System.out.println("this business login implementation got completed");
 	}
 }
